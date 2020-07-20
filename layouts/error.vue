ee239622d2dc497eca7079fe27a36a39eccb96d6
pm-ribeiro <pmribeiro@inf.ufpel.edu.br>
2020-07-20 19:31:49 -0300

    add no bk error image

--- layouts/error.vue
+++ layouts/error.vue
@@ -19,7 +19,7 @@
     </div>
     <v-img
       v-if="isHydrated && $vuetify.breakpoint.mdAndUp"
-      :src="$representers.getAssetsImage('error_image_2.png')"
+      :src="$representers.getAssetsImage('error_image.png')"
       alt="Profile picture"
       class="alien"
       contain
